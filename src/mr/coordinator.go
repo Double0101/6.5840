28c0c99a8cefd3bc8dce8814721c1a4413699cb1
Double0101 <double0101@foxmail.com>
2023-07-10 17:13:45 +0800

    get one task and task done()

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -11,11 +11,14 @@ import "net/http"
 
 type Coordinator struct {
 	// Your definitions here.
-	Files    []string
-	NoReduce int
-	Finished bool
-	Mu       sync.Mutex
-	NoWorker int
+	Files       []string
+	NoReduce    int
+	NoFile      int
+	MapTasks    []Task
+	MapFinished bool
+	Finished    bool
+	Mu          sync.Mutex
+	NoWorker    int
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -33,6 +36,56 @@ func (c *Coordinator) Register(args *RegisterArgs, reply *RegisterReply) error {
 	defer c.Mu.Unlock()
 	reply.WorkerId = c.NoWorker
 	c.NoWorker += 1
+	return nil
+}
+
+func (c *Coordinator) mapTaskCanRun(idx int) bool {
+	if c.MapTasks[idx].TaskStatus == TASK_STATUS_READY {
+		return true
+	}
+	// TODO: task rerun
+	return false
+}
+
+func (c *Coordinator) DispatchTask(args *TaskArgs, reply *TaskReply) error {
+	if !c.MapFinished {
+		// dispatch map task
+		idx := 0
+		for idx < c.NoFile {
+			c.Mu.Lock()
+			defer c.Mu.Unlock()
+			if c.mapTaskCanRun(idx) {
+				reply.TaskType = TASK_TYPE_MAP
+				reply.FilePath = c.Files[idx]
+				return nil
+			}
+			idx += 1
+		}
+	} else {
+		// dispatch reduce task
+	}
+	return nil
+}
+
+func (c *Coordinator) TaskDone(args *DoneArgs, reply *DoneReply) error {
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
+	c.MapTasks[args.TaskId].TaskStatus = TASK_STATUS_FINISH
+	c.MapTasks[args.TaskId].Result = args.Result
+
+	if c.MapFinished {
+		return nil
+	}
+
+	c.MapFinished = true
+	for _, task := range c.MapTasks {
+		if task.TaskStatus != TASK_STATUS_FINISH {
+			c.MapFinished = false
+			return nil
+		}
+	}
+	// TODO: if mapfinished == true uniform result
+
 	return nil
 }
 
@@ -70,10 +123,19 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 
 	// Your code here.
 	c.Files = files
+	c.NoFile = len(files)
 	c.NoReduce = nReduce
 	c.Mu = sync.Mutex{}
+	c.MapFinished = false
 	c.Finished = false
 	c.NoWorker = 0
+	c.MapTasks = make([]Task, len(files))
+	for i, f := range files {
+		c.MapTasks[i].TaskId = i
+		c.MapTasks[i].FilePath = f
+		c.MapTasks[i].TaskStatus = TASK_STATUS_READY
+		c.MapTasks[i].WorkerId = -1
+	}
 
 	c.server()
 	return &c
--- src/mr/util.go
+++ src/mr/util.go
@@ -1,10 +1,14 @@
 package mr
 
+import "time"
+
 type Task struct {
 	FilePath   string
 	TaskStatus int
 	WorkerId   int
 	TaskId     int
+	StartTime  time.Time
+	Result     []KeyValue
 }
 
 const (
@@ -15,8 +19,30 @@ const (
 	TASK_STATUS_ERROR   = 4
 )
 
+const (
+	TASK_TYPE_MAP    = 0
+	TASK_TYPE_REDUCE = 1
+)
+
 type RegisterArgs struct{}
 
 type RegisterReply struct {
 	WorkerId int
 }
+
+type TaskArgs struct {
+	WorkId int
+}
+
+type TaskReply struct {
+	FilePath string
+	TaskType int
+}
+
+type DoneArgs struct {
+	WorderId int
+	TaskId   int
+	Result   []KeyValue
+}
+
+type DoneReply struct{}
